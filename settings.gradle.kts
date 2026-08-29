58ce42dec2dccdf92e7680dbdb7b6874c7ea1f88
ydjd82604-glitch <ydjd82604@gmail.com>
2026-08-29 21:52:32 +0530

    Add settings.gradle.kts with plugin and dependency management

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+includePrebuilt = true
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JARVIS"
+include(":app")
